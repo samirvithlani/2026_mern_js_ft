9cfc621ce789a981d3d0202410aecebfbbed5e86
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-08 16:54:10 +0530

    box

---
+++ events/eventDemo3.js
@@ -0,0 +1,8 @@
+const getAllBoxes =()=>{
+    const boxes = document.getElementsByClassName("box") //array
+    console.log(boxes)
+    boxes[0].style.backgroundColor = "blue"
+
+    //even  box color red
+    //odd box color blue
+}
